75a51313068b227d2d7cdbb710c1c25f8e96f558
taylour <jayluvsdivn@gmail.com>
2023-04-28 20:53:15 -0700

    Converted existing code to be modular, new functions

--- deck.py
+++ deck.py
@@ -4,9 +4,40 @@ import random
 
 '''Standard 52 card playing deck generated during __init__ method,
     as well as some funtions for using the deck in a game'''
-class Deck_Standard_52:
+class Deck:
     def __init__(self):
         self.cards = []
+
+    def __str__(self):
+        "\n".join([str(card.__str__()) for card in self.cards])
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def add_multiple_cards(self, cards):
+        for card in cards:
+            self.add_card(card)
+
+    def deal_card(self):
+        return self.cards.pop()
+
+    # Not sure if this is working yet the way I intended
+    # More testing to make sure its playing nicely with
+    # add_multiple_cards function
+    def deal_multiple_cards(self, num_cards_dealt):
+        new = []
+        for i in range(num_cards_dealt):
+            new.append(self.deal_card())
+        return new
+    
+    def shuffle_deck(self):
+        random.shuffle(self.cards)
+
+    def show_cards(self):
+        for card in self.cards:
+            print(str(f"{card.face} of {card.suit}"))
+
+    def populate_standard_52(self):
         suits = ["Hearts", "Spades", "Diamonds", "Clubs"]
         faces = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, "Jack", "Queen", "King"]
         for suit in suits:
@@ -26,19 +57,11 @@ class Deck_Standard_52:
                 else:
                     card = Card(face, suit, value)
                     self.add_card(card)
-
-    def __str__(self):
-        "\n".join([str(card.__str__()) for card in self.cards])
-
-    def add_card(self, card):
-        self.cards.append(card)
-
-    def deal_card(self):
-        self.cards.pop()
-
-    def shuffle_deck(self):
-        random.shuffle(self.cards)
-
-    def show_cards(self):
-        for card in self.cards:
-            print(str(f"{card.face} of {card.suit}"))
+                    
+# For Debugging will remove
+deck = Deck()
+deck.populate_standard_52()
+deck.shuffle_deck()
+new = Deck()
+new.add_multiple_cards(deck.deal_multiple_cards(26))
+new.show_cards()
